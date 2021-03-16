4194ea29b450dede3d73b50092418de1ef81c53f
Thiago Giammattey <giammattey@gmail.com>
2021-03-16 11:20:10 -0300

    add transaction to api with MirageJS

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,9 @@ import Modal from 'react-modal'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+
+import { api } from '../../services/api'
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
@@ -23,12 +26,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    console.log({
+    const data = {
       title,
       value,
       type,
       category,
-    })
+    }
+
+    api.post('/transactions', data)
   }
 
   return (
