b50d878efe6740723544a795edf4e21c3ed7f0c3
Thiago Giammattey <giammattey@gmail.com>
2021-03-18 10:06:07 -0300

    refactor TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 import { NewTransactionModal } from './components/NewTransactionModal'
-import { TransactionsContext } from './TransactionsContext'
+import { TransactionsProvider } from './TransactionsContext'
 
 import { GlobalStyle } from './styles/global'
 
@@ -24,7 +24,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionalModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -32,6 +32,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       ></NewTransactionModal>
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   )
 }
